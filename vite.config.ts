86881c1fa5ba36b28c3d0109a9d6f1576d85ebd3
Koxx <koxx33@gmail.com>
2024-12-24 16:46:00 +0100

    lsp working

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,58 @@
-import react from '@vitejs/plugin-react'
-import type { UserConfig } from 'vite'
-import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2024 TypeFox and others.
+ * Licensed under the MIT License. See LICENSE in the package root for license information.
+ * ------------------------------------------------------------------------------------------ */
 
+import { defineConfig } from 'vite';
+import fs from 'node:fs';
+import * as path from 'node:path';
+import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin';
+import vsixPlugin from '@codingame/monaco-vscode-rollup-vsix-plugin';
+import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [react()],
-  optimizeDeps: {
-      esbuildOptions: {
-        plugins: [
-           
-          importMetaUrlPlugin]
-      }
+export const definedViteConfig = defineConfig({
+    build: {
+        target: 'ES2022',
+    },
+    resolve: {
+        // not needed here, see https://github.com/TypeFox/monaco-languageclient#vite-dev-server-troubleshooting
+        // dedupe: ['vscode']
+    },
+    server: {
+        origin: 'http://localhost:20001',
+        port: 20001,
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        },
+        watch: {
+            ignored: [
+                '**/profile/**/*'
+            ]
+        }
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                importMetaUrlPlugin
+            ]
+        },
+        include: [
+            'vscode-textmate',
+        ]
+    },
+    plugins: [
+        vsixPlugin(),
+        react()
+    ],
+    define: {
+        // rootDirectory: JSON.stringify(__dirname),
+        // Server-provided Content-Length header may be gzipped, get the real size in build time
+        // __WASM_SIZE__: fs.existsSync(clangdWasmLocation) ? fs.statSync(clangdWasmLocation).size : 0
+    },
+    worker: {
+        format: 'es'
     }
-} satisfies UserConfig
+});
+
+export default definedViteConfig;
